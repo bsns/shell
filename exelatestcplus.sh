ef60b9fa9aceae76597567ac737d537b1acd5807
bsns <bsns@live.com>
2016-06-08 16:02:46 +0800

    block error message

--- exelatestcplus.sh
+++ exelatestcplus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm a.out
+rm a.out >/dev/null 2>&1
 var=`ls -t *.cc | head -1` //find the latest cplus code
 grep -q "#include<iostream>" $var||sed -i '1 i\#include<iostream>' $var
 grep -q "using namespace std;" $var||sed -i '2 i\using namespace std;' $var

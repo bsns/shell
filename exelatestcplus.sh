c695d638f2fc5b3682b4b0316983de5c165a97eb
bsns <bsns@live.com>
2016-06-08 17:26:20 +0800

    fix

--- exelatestcplus.sh
+++ exelatestcplus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm a.out >/dev/null 2>&1
-var=`ls -t *.cc | head -1` //find the latest cplus code
+var=`ls -t *.cc | head -1`
+*find the latest cplus code
 grep -q "#include<iostream>" $var||sed -i '1 i\#include<iostream>' $var
 grep -q "using namespace std;" $var||sed -i '2 i\using namespace std;' $var
 g++ -std=c++17 $var
